be612863b4940e1bdccbfed462bf37e9a20d42a4
SwiftNCloak <markyjamesbonifacio@gmail.com>
2022-10-30 20:01:32 +0800

    Day-001 Complete

---
+++ day-001/main.py
@@ -0,0 +1,4 @@
+print("Welcome to the Swifty show!")
+country = input("What is your country of origin?\n")
+pet = input("What is your favorite pet?\n")
+print("Your superhero name is " + country + " " + pet + ".")
