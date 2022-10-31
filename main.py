6d963939041a3930bcae86d83996348ceedd7ad3
SwiftNCloak <markyjamesbonifacio@gmail.com>
2022-10-31 10:37:33 +0800

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-print("Welcome to the tip calculator!")
-totalBill = float(input("What is the total bill amount? \n$: "))
-tip = int(input("How much tip would you like to give? \nPercent: "))
-peopleSplit = int(input("How many people to split the bill? \nPeople: "))
-
-calcAns = ("{:.2f}".format((((totalBill * (tip / 100)) + totalBill) / peopleSplit)))
-print(f"Each person should pay: ${calcAns}")
